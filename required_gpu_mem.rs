332d72ae95ed0c94764a05ae567913f4cdddbe49
antibetagal <ticon08@gmail.com>
2025-05-26 16:44:16 -0700

    Commented by Phi4 with RTX3060 12GB

--- required_gpu_mem.rs
+++ required_gpu_mem.rs
@@ -1,47 +1,27 @@
-// This line tells Rust that we're using a library called `std::env`.
-use std::env;
-
+use std::env; // Importing the `env` module from the standard library.
 fn main() {
-    // This line gets the first argument passed to our program, which represents its name.
-    let app_name = env::args().nth(0);
+    let app_name = env::args().nth(0); // Getting the first argument (program name) using `nth(0)` on the iterator returned by `env::args()`.
 
-    // This line gets the second argument passed to our program. In Rust, arguments are zero-indexed,
-    // so `nth(1)` gets the second argument.
-    let arg = env::args().nth(1);
-
-    // This line prints out the name of our program using `app_name`. The `.unwrap_or_default()` method
-    // is used to handle the case where `app_name` is `None`, in which case it returns an empty string.
+    let arg = env::args().nth(1); // Getting the second argument (user provided number) using `nth(1)` on the same iterator as above.
     println!("Program name: {}", app_name.unwrap_or_default());
+    // Printing out the program name. If it is `None`, the `unwrap_or_default()` method returns an empty string.
 
-    // This match statement checks whether we have any arguments passed to our program. If we do, it's
-    // stored in the `arg` variable. The `Some(ref arg)` arm of the match is used to pattern-match on
-    // the value of `arg`. The `ref` keyword is used to indicate that we want to borrow the value,
-    // rather than taking ownership of it.
     match arg {
-        // If `arg` is `Some`, this code will run. It prints out a message indicating that we passed an argument,
-        // and includes the value of the argument in GB (using `B` as a unit suffix).
-        Some(ref arg) => println!("You passed: {}B", arg),
+        Some(ref arg) => println!("You passed: {}B", arg), // If `arg` has a value, print "You passed: {value}B".
 
-        // If `arg` is `None`, this arm of the match will run. It prints out an error message
-        // indicating that we need to pass a number, and then exits our program using
-        // `std::process::exit(1)`. The `(1)` at the end indicates that we're exiting with a status code
-        // of 1, which typically means "error".
-        None => { println!("Provide number (B) of params."); 
+        None => {
+            println!("Provide number (B) of params."); // If `arg` is `None`, print an error message and exit the program with status code 1.
             std::process::exit(1);  
         }
     }
 
-    // This line attempts to parse the value of `arg` into a floating-point number. The `.expect("No argument.")`
-    // method is used to handle any errors that might occur during parsing, and will exit our program with an error
-    // message if parsing fails.
     let num: f32 = arg.expect("No argument.").parse::<f32>().unwrap_or_default();
+    // Parse the string value of `arg` into a floating-point number (`f32`). If parsing fails, return a default value of 0.0.
+    // The `expect()` method is used to panic with an error message if `arg` is `None`.
 
-    // This line calculates the amount of GPU memory we would have available for a given number of parameters.
-    // The calculation is based on the formula `(num * 4.0) / (32.0 / 16.0) * 1.2`. It's not entirely clear
-    // what this formula represents, but it appears to be calculating some kind of memory allocation based on
-    // the number of parameters.
     let gpu_mem = (num * 4.0) / (32.0 / 16.0) * 1.2;
+    // Calculate the GPU memory using the formula `(num * 4.0) / (32.0 / 16.0) * 1.2`.
 
-    // This line prints out the result of our calculation in GB (using `GB` as a unit suffix).
     println!("GPU Memory: {} GB", gpu_mem);
+    // Print out the calculated GPU memory.
 }
